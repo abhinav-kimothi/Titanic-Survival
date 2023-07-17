e806c72309bd3da4220a54f772139e011987b9fa
Abhinav Kimothi <118165775+abhinav-kimothi@users.noreply.github.com>
2023-07-18 00:06:27 +0530

    updated streamlit app

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pickle
 from configparser import ConfigParser
 import utils
 import base64
+import json
 
 #-----Reading Config File---------#
 
@@ -17,7 +18,14 @@ header_image=config_object["IMAGES"]["header_image"]
 #----------------------------------#
 
 #-----Loading Model----------------#
-model_path=config_object["MODEL"]["model_path"]
+model_path_logreg=config_object["MODEL"]["model_path_logreg"]
+model_path_rf=config_object["MODEL"]["model_path_rfc"]
+model_path_xgb=config_object["MODEL"]["model_path_xgb"]
+model_path_bc=config_object["MODEL"]["model_path_bc"]
+
+with open("./Data/metrics_logreg.json","r") as file:
+    metrics_logreg=json.load(file)
+
 
 
 #----------------------Setting Page Config------------------------#
@@ -40,17 +48,16 @@ with st.sidebar.container():
     col1,col2,col3=st.columns([1,7,1])
     with col2:
         #----Placing Logo-----#
-        st.sidebar.image(logo)
-        st.sidebar.image('./Assets/sidebanner.png')
+        #st.sidebar.image(logo)
+        st.sidebar.image('./Assets/HeaderImage.png')
+
 
 
 
 
     
 
-st.sidebar.markdown("*Get In Touch!*")
-with st.sidebar.container():
-     utils.sidebar_footer()
+
 
 
 
@@ -68,25 +75,100 @@ email_icon=config_object["Contact"]["email_icon"]
 github_repo=config_object["Contact"]["github_repo"]
 github_icon=config_object["Contact"]["github_icon"]
 
+with st.sidebar.container():
+    st.divider()
+    model_selection=st.radio("Select Model",("Logistic Regression","Random Forest","XGBoost","Bagging Classifier"))
+    if model_selection=="Logistic Regression":
+        model_path=model_path_logreg
+        with st.expander("View Model Metrics"):
+            with open("./Data/metrics_logreg.json","r") as file:
+                metrics=json.load(file)
+            st.write("Training Accuracy:"+str(metrics["model"][0]["accuracy_score_train"])+"%")
+            st.write("Validation Accuracy:"+str(metrics["model"][0]["accuracy_score_test"])+"%")
+            st.write("Validation Precision:"+str(metrics["model"][0]["precision_score_test"])+"%")
+            st.write("Validation Recall:"+str(metrics["model"][0]["recall_score_test"])+"%")
+            st.write("Validation F1 Score:"+str(metrics["model"][0]["f1_score_test"])+"%")
+            st.write("Confusion Matrix")
+            st.image("./Assets/cm_logreg_best.png")
+            st.write("ROC Curve")
+            st.image("./Assets/roc_logreg_best.png")
+            st.write("ROC AUC Score:"+str(metrics["model"][0]["roc_auc_score_test"])+"%")
+    elif model_selection=="Random Forest":
+        model_path=model_path_rf
+        with st.expander("View Model Metrics"):
+            with open("./Data/metrics_rfc.json","r") as file:
+                metrics=json.load(file)
+            st.write("Training Accuracy:"+str(metrics["model"][0]["accuracy_score_train"])+"%")
+            st.write("Validation Accuracy:"+str(metrics["model"][0]["accuracy_score_test"])+"%")
+            st.write("Validation Precision:"+str(metrics["model"][0]["precision_score_test"])+"%")
+            st.write("Validation Recall:"+str(metrics["model"][0]["recall_score_test"])+"%")
+            st.write("Validation F1 Score:"+str(metrics["model"][0]["f1_score_test"])+"%")
+            st.write("Confusion Matrix")
+            st.image("./Assets/cm_rfc_best.png")
+            st.write("ROC Curve")
+            st.image("./Assets/roc_rfc_best.png")
+            st.write("ROC AUC Score:"+str(metrics["model"][0]["roc_auc_score_test"])+"%")
 
-with st.container():
-    col1,col2,col3=st.columns([1,10,1])
-    with col2:
+    elif model_selection=="XGBoost":
+        model_path=model_path_xgb
+        with st.expander("View Model Metrics"):
+            with open("./Data/metrics_xgb.json","r") as file:
+                metrics=json.load(file)
+            st.write("Training Accuracy:"+str(metrics["model"][0]["accuracy_score_train"])+"%")
+            st.write("Validation Accuracy:"+str(metrics["model"][0]["accuracy_score_test"])+"%")
+            st.write("Validation Precision:"+str(metrics["model"][0]["precision_score_test"])+"%")
+            st.write("Validation Recall:"+str(metrics["model"][0]["recall_score_test"])+"%")
+            st.write("Validation F1 Score:"+str(metrics["model"][0]["f1_score_test"])+"%")
+            st.write("Confusion Matrix")
+            st.image("./Assets/cm_xgb_best.png")
+            st.write("ROC Curve")
+            st.image("./Assets/roc_xgb_best.png")
+            st.write("ROC AUC Score:"+str(metrics["model"][0]["roc_auc_score_test"])+"%")
+
+    elif model_selection=="Bagging Classifier":
+        model_path=model_path_bc
+        with st.expander("View Model Metrics"):
+            with open("./Data/metrics_bc.json","r") as file:
+                metrics=json.load(file)
+            st.write("Training Accuracy:"+str(metrics["model"][0]["accuracy_score_train"])+"%")
+            st.write("Validation Accuracy:"+str(metrics["model"][0]["accuracy_score_test"])+"%")
+            st.write("Validation Precision:"+str(metrics["model"][0]["precision_score_test"])+"%")
+            st.write("Validation Recall:"+str(metrics["model"][0]["recall_score_test"])+"%")
+            st.write("Validation F1 Score:"+str(metrics["model"][0]["f1_score_test"])+"%")
+            st.write("Confusion Matrix")
+            st.image("./Assets/cm_bc_best.png")
+            st.write("ROC Curve")
+            st.image("./Assets/roc_bc_best.png")
+            st.write("ROC AUC Score:"+str(metrics["model"][0]["roc_auc_score_test"])+"%")
+
+
+#with st.container():
+    #col1,col2,col3=st.columns([1,10,1])
+    #with col2:
         #----Header Image-----#
-        st.image(header_image)
+        #st.image(header_image)
         #----Social Links-----#
-        st.markdown(f"<h6 style='text-align:right;'><a href={github_repo}><img src={github_icon} alt='github' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={linkedin_link}><img src={linkedin_icon} alt='linkedin' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={twitter_link}><img src={twitter_icon} alt='twitter' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={email_link}><img src={email_icon} alt='email' style='width:42px;height:42px;'></a></h6>",unsafe_allow_html=True)
+        #st.markdown(f"<h6 style='text-align:right;'><a href={github_repo}><img src={github_icon} alt='github' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={linkedin_link}><img src={linkedin_icon} alt='linkedin' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={twitter_link}><img src={twitter_icon} alt='twitter' style='width:42px;height:42px;'></a>&emsp;&emsp;<a href={email_link}><img src={email_icon} alt='email' style='width:42px;height:42px;'></a></h6>",unsafe_allow_html=True)
 
 with st.container():
-    col1,col2=st.columns(2)
+    col1,col2=st.columns([1,1])
     with col1:
-        with st.container():
-            p_class,embarkment,gender,age,fare,family_size,siblings,submit=utils.input_form()
-
+            title,name,p_class,embarkment,gender,age,fare,family_size,siblings,submit=utils.input_form()
     with col2:
-        with st.container():
-            prediction=utils.predict_survival(p_class,embarkment,gender,age,fare,family_size,siblings,model_path)
-            st.write(prediction)
+        if submit==True:
+            prediction,probability=utils.predict_survival(p_class,embarkment,gender,age,fare,family_size,siblings,model_path)
+            if prediction=="Survived":
+                st.markdown(f" #### <span style='color:#FE633D;'>💜 The passenger {title} {name} is predicted to have survived with a survival probability of "+str(probability)+"%</span>",unsafe_allow_html=True)
+                st.image("./Assets/alive.png")
+            else:
+                st.markdown(f" #### <span style='color:#FE633D;'>🧟‍♂️ The passenger {title} {name} is predicted to have not survived with a survival probability of only "+str(probability)+"%</span>",unsafe_allow_html=True)
+                st.image("./Assets/dead.png")
+            
+        else:
+            st.markdown(" #### <span style='color:#FE633D;'>👈🏼 Please enter the passenger details and click on the submit button to get the prediction</span>",unsafe_allow_html=True)
 
 
-
+st.sidebar.divider()
+st.sidebar.markdown("*Get In Touch!*")
+with st.sidebar.container():
+     utils.sidebar_footer()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import pickle
+import joblib
 from configparser import ConfigParser
+import pandas as pd
+import numpy as np
 
 #-----Reading Config File---------#
 
@@ -33,180 +35,173 @@ def load_css_file(css_file_path):
 
 def input_form():
     with st.form("Input",clear_on_submit=False):
+        title=st.selectbox("Title",["Mr","Mrs","Miss","Master","Other"],index=2,help="Title of the passenger")
+        name=st.text_input("Name",help="Name of the passenger",max_chars=50,placeholder="Enter Name",value="Jane Doe")
         p_class=st.selectbox("Passenger Class",["First","Second","Third"])
         embarkment=st.selectbox("Port of Embarkment",["Cherbourg","Queenstown","Southampton"])
         gender=st.radio("Gender",["Male","Female"],horizontal=True)
 
-        age=st.slider("Age",min_value=0,max_value=100,value=0,step=1)
-        fare=st.slider("Fare",min_value=0.0,max_value=600.0,value=0.0,step=0.1)
+        age=st.slider("Age",min_value=0,max_value=100,value=40,step=1)
+        fare=st.slider("Fare",min_value=0.0,max_value=50.0,value=10.0,step=0.1,format="%.2f",help="Fare paid by the passenger")
 
         family_size=st.number_input("Parents/Children",min_value=0,max_value=10,value=0,step=1)
         siblings=st.number_input("Siblings/Spouses",min_value=0,max_value=10,value=0,step=1)    
 
         submit=st.form_submit_button("Submit",use_container_width=True)
 
-    return p_class,embarkment,gender,age,fare,family_size,siblings,submit
+    return title,name,p_class,embarkment,gender,age,fare,family_size,siblings,submit
 
-def predict_survival(p_class,embarkment,gender,age,fare,family_size,siblings,model):
-    if p_class=="First":
-        class1=1
-        class2=0
-        class3=0
-    elif p_class=="Second":
-        class1=0
-        class2=1
-        class3=0
-    else:
-        class1=0
-        class2=0
-        class3=1
-    if embarkment=="Cherbourg":
-        emb_c=1
-        em_q=0
-        em_s=0
-    elif embarkment=="Queenstown":
-        emb_c=0
-        em_q=1
-        em_s=0
-    else:
-        emb_c=0
-        em_q=0
-        em_s=1
-    if gender=="Male":
-        male=1
-        female=0
-    else:
-        female=1
-        male=0
+def predict_survival(p_class="First",embarkment="S",gender="Male",age=40,fare=10,family_size=1,siblings=1,model='./Models/model_logreg_best.joblib',title="Miss"):
+    example_df=pd.DataFrame(columns=['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'log_Fare', 'FamilySize',
+       'Sex_female', 'Sex_male', 'Embarked_C', 'Embarked_Q', 'Embarked_S',
+       'Title_Master', 'Title_Miss', 'Title_Mr', 'Title_Mrs', 'Title_Other',
+       'AgeGroup_Child', 'AgeGroup_Teenager', 'AgeGroup_Adult',
+       'AgeGroup_MiddleAge', 'AgeGroup_Senior', 'FareBin_0-8', 'FareBin_8-10',
+       'FareBin_10-20', 'FareBin_20-40', 'FareBin_40+'],index=[0])
     
-    if family_size==0: 
-        parch_0=1
-        parch_1=0
-        parch_2=0
-        parch_3=0
-        parch_4=0
-        parch_5=0
-        parch_6=0
-    elif family_size==1: 
-        parch_1=1
-        parch_0=0
-        parch_2=0
-        parch_3=0
-        parch_4=0
-        parch_5=0
-        parch_6=0
-    elif family_size==2: 
-        parch_2=1
-        parch_0=0
-        parch_1=0
-        parch_3=0
-        parch_4=0
-        parch_5=0
-        parch_6=0
-    elif family_size==3: 
-        parch_3=1
-        parch_0=0
-        parch_1=0
-        parch_2=0
-        parch_4=0
-        parch_5=0
-        parch_6=0
-    elif family_size==4: 
-        parch_4=1
-        parch_0=0
-        parch_1=0
-        parch_2=0
-        parch_3=0
-        parch_5=0
-        parch_6=0
-    elif family_size==5: 
-        parch_5=1
-        parch_0=0
-        parch_1=0
-        parch_2=0
-        parch_3=0
-        parch_4=0
-        parch_6=0
-    elif family_size==6: 
-        parch_6=1
-        parch_0=0
-        parch_1=0
-        parch_2=0
-        parch_3=0
-        parch_4=0
-        parch_5=0
-    else: 
-        parch_6=1
-        parch_0=0
-        parch_1=0
-        parch_2=0
-        parch_3=0
-        parch_4=0
-        parch_5=0
-    if siblings==0: 
-        sib_0=1
-        sib_1=0
-        sib_2=0
-        sib_3=0
-        sib_4=0
-        sib_5=0
-        sib_8=0
-    elif siblings==1: 
-        sib_1=1
-        sib_0=0
-        sib_2=0
-        sib_3=0
-        sib_4=0
-        sib_5=0
-        sib_8=0
-    elif siblings==2: 
-        sib_2=1
-        sib_0=0
-        sib_1=0
-        sib_3=0
-        sib_4=0
-        sib_5=0
-        sib_8=0
-    elif siblings==3: 
-        sib_3=1
-        sib_0=0
-        sib_1=0
-        sib_2=0
-        sib_4=0
-        sib_5=0
-        sib_8=0
-    elif siblings==4:   
-        sib_4=1
-        sib_0=0
-        sib_1=0
-        sib_2=0
-        sib_3=0
-        sib_5=0
-        sib_8=0
-    elif siblings==5:
-        sib_5=1
-        sib_0=0
-        sib_1=0
-        sib_2=0
-        sib_3=0
-        sib_4=0
-        sib_8=0
-    else: 
-        sib_8=1
-        sib_0=0
-        sib_1=0
-        sib_2=0
-        sib_3=0
-        sib_4=0
-        sib_5=0
 
-    pickle_model=pickle.load(open(model,'rb'))
+    if p_class=="First":
+        example_df.Pclass=1
+    elif p_class=="Second":
+        example_df.Pclass=2
+    else:
+        example_df.Pclass=3
+    if gender=='Male':
+        example_df.Sex_male=1
+        example_df.Sex_female=0
+    else:
+        example_df.Sex_male=0
+        example_df.Sex_female=1
+
+    if embarkment=='S':
+        example_df.Embarked_S=1
+        example_df.Embarked_C=0
+        example_df.Embarked_Q=0
+    elif embarkment=='C':
+        example_df.Embarked_S=0
+        example_df.Embarked_C=1
+        example_df.Embarked_Q=0
+    else:
+        example_df.Embarked_S=0
+        example_df.Embarked_C=0
+        example_df.Embarked_Q=1
+
+    if title=='Mr':
+        example_df.Title_Mr=1
+        example_df.Title_Mrs=0
+        example_df.Title_Miss=0
+        example_df.Title_Master=0
+        example_df.Title_Other=0
+    elif title=='Mrs':
+        example_df.Title_Mr=0
+        example_df.Title_Mrs=1
+        example_df.Title_Miss=0
+        example_df.Title_Master=0
+        example_df.Title_Other=0
+    elif title=='Miss':
+        example_df.Title_Mr=0
+        example_df.Title_Mrs=0
+        example_df.Title_Miss=1
+        example_df.Title_Master=0
+        example_df.Title_Other=0
+    elif title=='Master':
+        example_df.Title_Mr=0
+        example_df.Title_Mrs=0
+        example_df.Title_Miss=0
+        example_df.Title_Master=1
+        example_df.Title_Other=0
+    else:
+        example_df.Title_Mr=0
+        example_df.Title_Mrs=0
+        example_df.Title_Miss=0
+        example_df.Title_Master=0
+        example_df.Title_Other=1
+
+    if age<12:
+        example_df.AgeGroup_Child=1
+        example_df.AgeGroup_Teenager=0
+        example_df.AgeGroup_Adult=0
+        example_df.AgeGroup_MiddleAge=0
+        example_df.AgeGroup_Senior=0
+    elif age<20:
+        example_df.AgeGroup_Child=0
+        example_df.AgeGroup_Teenager=1
+        example_df.AgeGroup_Adult=0
+        example_df.AgeGroup_MiddleAge=0
+        example_df.AgeGroup_Senior=0
+    elif age<40:
+        example_df.AgeGroup_Child=0
+        example_df.AgeGroup_Teenager=0
+        example_df.AgeGroup_Adult=1
+        example_df.AgeGroup_MiddleAge=0
+        example_df.AgeGroup_Senior=0
+    elif age<60:
+        example_df.AgeGroup_Child=0
+        example_df.AgeGroup_Teenager=0
+        example_df.AgeGroup_Adult=0
+        example_df.AgeGroup_MiddleAge=1
+        example_df.AgeGroup_Senior=0
+    else:
+        example_df.AgeGroup_Child=0
+        example_df.AgeGroup_Teenager=0
+        example_df.AgeGroup_Adult=0
+        example_df.AgeGroup_MiddleAge=0
+        example_df.AgeGroup_Senior=1
+
+    if fare<8:
+        example_df['FareBin_0-8']=1
+        example_df['FareBin_8-10']=0
+        example_df['FareBin_10-20']=0
+        example_df['FareBin_20-40']=0
+        example_df['FareBin_40+']=0
+    elif fare<10:
+        example_df['FareBin_0-8']=0
+        example_df['FareBin_8-10']=1
+        example_df['FareBin_10-20']=0
+        example_df['FareBin_20-40']=0
+        example_df['FareBin_40+']=0
+    elif fare<20:
+        example_df['FareBin_0-8']=0
+        example_df['FareBin_8-10']=0
+        example_df['FareBin_10-20']=1
+        example_df['FareBin_20-40']=0
+        example_df['FareBin_40+']=0
+    elif fare<40:
+        example_df['FareBin_0-8']=0
+        example_df['FareBin_8-10']=0
+        example_df['FareBin_10-20']=0
+        example_df['FareBin_20-40']=1
+        example_df['FareBin_40+']=0
+    else:
+        example_df['FareBin_0-8']=0
+        example_df['FareBin_8-10']=0
+        example_df['FareBin_10-20']=0
+        example_df['FareBin_20-40']=0
+        example_df['FareBin_40+']=1
+
+    example_df.Age = age
+    example_df.SibSp = siblings
+    example_df.Parch = family_size
+    example_df.Fare = fare
+    example_df.log_Fare = np.log(fare) if fare > 0 else 0
+    example_df.FamilySize = siblings + family_size + 1
+
+
+    selected_model=joblib.load(open(model,'rb'))
 
 
 
-    prediction=pickle_model.predict([[age, fare, parch_0, parch_1, parch_2, parch_3, parch_4,parch_5, parch_6, emb_c, em_q, em_s, female, male,class1, class2, class3, sib_0, sib_1, sib_2, sib_3,sib_4, sib_5, sib_8]])
+    prediction = selected_model.predict(example_df)
 
-    return prediction
+    # Get the survival probability
+    survival_probability = selected_model.predict_proba(example_df)[:, 1]
+
+    # Map the prediction to the corresponding class label
+    prediction = 'Survived' if prediction == 1 else 'Did not survive'
+
+    return prediction, round(survival_probability[0]*100, 2)
+
+
 
 def sidebar_footer():
     col1,col2,col3=st.columns(3)
@@ -220,3 +215,5 @@ def sidebar_footer():
         st.markdown(f"<span style='text-align:center;'><a href={feedback_link}><img src={feedback_icon} alt='coffee' style='width:40px;height:40px;' title='Share Feedback'></a></span>",unsafe_allow_html=True)
         #st.markdown("<p style='font-size:12px;text-align:left;'>Feedback</p>",unsafe_allow_html=True)
     st.markdown(f"<h6 style='text-align:left;'><a href={github_repo}><img src={github_icon} alt='github' style='width:42px;height:42px;'></a>&emsp;<a href={linkedin_link}><img src={linkedin_icon} alt='linkedin' style='width:42px;height:42px;'></a>&emsp;<a href={twitter_link}><img src={twitter_icon} alt='twitter' style='width:42px;height:42px;'></a>&emsp;<a href={email_link}><img src={email_icon} alt='email' style='width:42px;height:42px;'></a>&emsp;<a href={youtube_link}><img src={youtube_icon} alt='email' style='width:42px;height:42px;'></a></h6>",unsafe_allow_html=True)
+    st.divider()
+    st.image('./Assets/AKAIlogoblacknobg.jpg')
